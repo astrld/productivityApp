ce8c1082cf5408c358ba176d87418cb70a1f6b0e
agrawal6595 <agrawal6595@students.d211.org>
2023-09-19 10:10:11 -0500

    Workout app

--- app/src/main/java/com/example/workoutapp/main.java
+++ app/src/main/java/com/example/workoutapp/main.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.ColorRes;
@@ -14,6 +16,10 @@ public class main extends AppCompatActivity{
     private DBHandler dbHandler;
     public static boolean hasDB = false;
     private TextView displayName;
+
+    private ImageView home, calendar, graph, profile, setting;
+
+    private LinearLayout bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +27,38 @@ public class main extends AppCompatActivity{
         setStatusBarColor(R.color.beige);
         dbHandler = Database.getDBHandler();
         displayName = findViewById(R.id.displayName);
+
+        home = findViewById(R.id.home);
+        calendar = findViewById(R.id.calendar);
+        graph = findViewById(R.id.graph);
+        profile = findViewById(R.id.profile);
+        setting = findViewById(R.id.setting);
+
+        home.setMinimumWidth(home.getHeight());
+        calendar.setMinimumWidth(calendar.getHeight());
+        graph.setMinimumWidth(graph.getHeight());
+        profile.setMinimumWidth(profile.getHeight());
+        setting.setMinimumWidth(setting.getHeight());
+
+        bottomNav = findViewById(R.id.bottomNav);
+        double width = bottomNav.getWidth() - 20;
+        // make all icons equidistant from each other and they should cover the entire linear layout
+        double x = width/5;
+        home.setX((float) (x/2));
+        calendar.setX((float) (x/2 + x));
+        graph.setX((float) (x/2 + 2*x));
+        profile.setX((float) (x/2 + 3*x));
+        setting.setX((float) (x/2 + 4*x));
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 displayName.setText("Hello, " + dbHandler.getFirstName() + " " + dbHandler.getLastName());
+                System.out.println("Width of bottomNav: " + bottomNav.getWidth());
+                System.out.println("Height of bottomNav: " + bottomNav.getHeight());
+                System.out.println("Width of home: " + home.getWidth());
+                System.out.println("Height of home: " + home.getHeight());
             }
-        }, 1);
+        }, 100);
     }
 
     private void setStatusBarColor(@ColorRes int colorRes) {
