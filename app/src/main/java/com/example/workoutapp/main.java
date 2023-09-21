d29b9680d86069e85f99c12a6ef2f274d13fc794
agrawal6595 <agrawal6595@students.d211.org>
2023-09-21 09:01:56 -0500

    Workout app

--- app/src/main/java/com/example/workoutapp/main.java
+++ app/src/main/java/com/example/workoutapp/main.java
@@ -1,83 +1,67 @@
 package com.example.workoutapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentContainerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class main extends AppCompatActivity{
     private DBHandler dbHandler;
     private TextView displayName;
 
-    private FragmentContainerView bottomNav;
-
     private int currentView = 1;
+    private BottomNavigationView bottomNavigationView;
 
-    private ImageView home, calendar, graph, profile, setting;
+    private homeFragment HomeFragment = new homeFragment();
 
+    private calendarFragment CalendarFragment = new calendarFragment();
+
+    private graphFragment GraphFragment = new graphFragment();
+
+    private profileFragment ProfileFragment = new profileFragment();
+
+    private settingsFragment SettingsFragment = new settingsFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.main);
         setStatusBarColor(R.color.beige);
         dbHandler = Database.getDBHandler();
-        displayName = findViewById(R.id.displayName);
-        bottomNav = findViewById(R.id.fragment_container_view);
-        new Handler().postDelayed(new Runnable() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, HomeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void run() {
-                displayName.setText("Hello, " + dbHandler.getFirstName() + " " + dbHandler.getLastName());
-                home = bottomNav.findViewById(R.id.home);
-                calendar = bottomNav.findViewById(R.id.calendar);
-                graph = bottomNav.findViewById(R.id.graph);
-                profile = bottomNav.findViewById(R.id.profile);
-                setting = bottomNav.findViewById(R.id.setting);
-                home.setColorFilter(getResources().getColor(R.color.beige2));
-                calendar.setColorFilter(getResources().getColor(R.color.black));
-                graph.setColorFilter(getResources().getColor(R.color.black));
-                profile.setColorFilter(getResources().getColor(R.color.black));
-                setting.setColorFilter(getResources().getColor(R.color.black));
-                home.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switchToHome();
-                    }
-                });
-                calendar.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switchToCalendar();
-                    }
-                });
-                graph.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switchToGraph();
-                    }
-                });
-                profile.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switchToProfile();
-                    }
-                });
-                setting.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switchToSetting();
-                    }
-                });
+            public boolean onNavigationItemSelected(MenuItem item) {
+                // should get it from @menu/bottom_navigation_menu
+                switch (item.getItemId()){
+                    case R.id.homeMenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, HomeFragment).commit();
+                        return true;
+                    case R.id.calendarMenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, CalendarFragment).commit();
+                        return true;
+                    case R.id.graphMenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, GraphFragment).commit();
+                        return true;
+                    case R.id.profileMenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, ProfileFragment).commit();
+                        return true;
+                    case R.id.settingsMenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, SettingsFragment).commit();
+                        return true;
+                }
+                return false;
             }
-        }, 100);
+        });
+
     }
 
     private void setStatusBarColor(@ColorRes int colorRes) {
@@ -88,124 +72,5 @@ public class main extends AppCompatActivity{
         }
     }
 
-    private void updateBottomNav(){
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if(currentView == 1){
-                    bottomNav = findViewById(R.id.fragment_container_view);
-                }else if(currentView == 2){
-                    bottomNav = findViewById(R.id.fragment_container_view1);
-                }
-                else if(currentView == 3){
-                    bottomNav = findViewById(R.id.fragment_container_view2);
-                }
-                else if(currentView == 4){
-                    bottomNav = findViewById(R.id.fragment_container_view3);
-                }
-                else if(currentView == 5){
-                    bottomNav = findViewById(R.id.fragment_container_view4);
-                }
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        home = bottomNav.findViewById(R.id.home);
-                        calendar = bottomNav.findViewById(R.id.calendar);
-                        graph = bottomNav.findViewById(R.id.graph);
-                        profile = bottomNav.findViewById(R.id.profile);
-                        setting = bottomNav.findViewById(R.id.setting);
-                        home.setColorFilter(getResources().getColor(R.color.black));
-                        calendar.setColorFilter(getResources().getColor(R.color.black));
-                        graph.setColorFilter(getResources().getColor(R.color.black));
-                        profile.setColorFilter(getResources().getColor(R.color.black));
-                        setting.setColorFilter(getResources().getColor(R.color.black));
-                        if(currentView == 1){
-                            home.setColorFilter(getResources().getColor(R.color.beige2));
-                        }else if(currentView == 2){
-                            calendar.setColorFilter(getResources().getColor(R.color.beige2));
-                        }
-                        else if(currentView == 3){
-                            graph.setColorFilter(getResources().getColor(R.color.beige2));
-                        }
-                        else if(currentView == 4){
-                            profile.setColorFilter(getResources().getColor(R.color.beige2));
-                        }
-                        else if(currentView == 5){
-                            setting.setColorFilter(getResources().getColor(R.color.beige2));
-                        }
-                        home.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                switchToHome();
-                            }
-                        });
-                        calendar.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                switchToCalendar();
-                            }
-                        });
-                        graph.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                switchToGraph();
-                            }
-                        });
-                        profile.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                switchToProfile();
-                            }
-                        });
-                        setting.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                switchToSetting();
-                            }
-                        });
-                    }
-                }, 10);
-            }
-        }, 10);
-    }
-    private void switchToHome(){
-        if(currentView != 1) {
-            setContentView(R.layout.home);
-            currentView = 1;
-            updateBottomNav();
-        }
-    }
-
-    private void switchToCalendar(){
-        if(currentView != 2){
-            setContentView(R.layout.calendar);
-            currentView = 2;
-            updateBottomNav();
-        }
-    }
-
-    private void switchToGraph(){
-        if(currentView != 3){
-            setContentView(R.layout.graph);
-            currentView = 3;
-            updateBottomNav();
-        }
-    }
-
-    private void switchToProfile(){
-        if(currentView != 4){
-            setContentView(R.layout.profile);
-            currentView = 4;
-            updateBottomNav();
-        }
-    }
-
-    private void switchToSetting(){
-        if(currentView != 5){
-            setContentView(R.layout.settings);
-            currentView = 5;
-            updateBottomNav();
-        }
-    }
 
 }
--- app/src/main/java/com/example/workoutapp/settingsFragment.java
+++ app/src/main/java/com/example/workoutapp/settingsFragment.java
@@ -1,15 +1,17 @@
 package com.example.workoutapp;
 
-import android.app.Fragment;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class home extends Fragment {
+public class settingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
